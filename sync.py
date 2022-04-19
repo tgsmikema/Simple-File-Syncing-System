cf696c28ed72903ed292972462eeb204709ddca8
tgsmikema <tgsmikema@gmail.com>
2022-04-19 13:21:43 +0800

    encapsulated with main() function

--- sync.py
+++ sync.py
@@ -2,23 +2,19 @@ import os
 from datetime import datetime
 import hashlib
 import shutil
+import json
 
-path = os.getcwd() + "/" + "README.md"
 
-time = os.path.getmtime(path)
+def main():
+    path = os.getcwd() + "/" + "README.md"
+    time = os.path.getmtime(path)
+    time = time.__trunc__()
+    dt_c = datetime.fromtimestamp(time)
+    print(dt_c.astimezone())
+    dt = datetime.fromtimestamp(os.path.getmtime(path))
+    print(dt)
+    hashed_string = hashlib.sha256("ssss".encode('utf-8')).hexdigest()
+    print(hashed_string)
 
-time = time.__trunc__()
+main()
 
-dt_c = datetime.fromtimestamp(time)
-
-print(dt_c.astimezone())
-
-dt = datetime.fromtimestamp(os.path.getmtime(path))
-
-print(dt)
-
-hashed_string = hashlib.sha256("ssss".encode('utf-8')).hexdigest()
-print(hashed_string)
-
-print()
-print()
